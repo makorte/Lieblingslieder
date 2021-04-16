80263947e94edc28071da2e600df26357e2b31fc
Max Korte <privat@max-korte.de>
2021-04-16 11:06:16 +0200

    added getUserAction() and exit() in App

--- app/src/main/java/com/maxkorte/App.java
+++ app/src/main/java/com/maxkorte/App.java
@@ -9,6 +9,51 @@ public class App {
     public static void main(String[] args) {
         nutzer = initNutzer();
         printLieder();
+
+        int userAction = getUserAction();
+        switch (userAction){
+            case 1: addLied();
+            case 2: removeLied();
+            case 3: removeAll();
+            case 9: exit();
+            default: System.out.println("Fehler");
+        }
+    }
+
+    public static void addLied(){
+        // TODO: 16.04.21 implement 
+    }
+
+    public static void removeLied(){
+        // TODO: 16.04.21 implement 
+    }
+
+    public static void removeAll(){
+        // TODO: 16.04.21 implement 
+    }
+
+    public static void exit(){
+        System.out.println("Das Programm wird beendet...");
+        System.exit(0);
+    }
+
+    public static int getUserAction(){
+        int input = 0;
+
+        boolean running;
+        do {
+            System.out.println("1: Lied hinzufügen, 2: Lied entfernen, 3: alle Lieder entfernen, 9: Beenden");
+            try{
+                input = sc.nextInt();
+                if(input < 1 || (input > 3 && input < 9) || input > 9) throw new Exception();
+                running = false;
+            } catch (Exception e){
+                System.out.println("Falsche Eingabe!");
+                running = true;
+            }
+        } while (running);
+
+        return input;
     }
 
     public static Nutzer initNutzer(){
